115a8ae8b7f54730e5b40aacc9e7ce04537e4d0f
fengshuonan <sn93@qq.com>
2020-12-21 22:54:54 +0800

    【system】更新用户查询的问题

--- kernel-s-system/system-business-user/src/main/java/cn/stylefeng/roses/kernel/system/modular/user/pojo/response/SysUserResponse.java
+++ kernel-s-system/system-business-user/src/main/java/cn/stylefeng/roses/kernel/system/modular/user/pojo/response/SysUserResponse.java
@@ -17,7 +17,7 @@ public class SysUserResponse {
     /**
      * 主键
      */
-    private Long id;
+    private Long userId;
 
     /**
      * 账号
@@ -32,7 +32,7 @@ public class SysUserResponse {
     /**
      * 姓名
      */
-    private String name;
+    private String realName;
 
     /**
      * 头像
@@ -46,7 +46,7 @@ public class SysUserResponse {
     private Date birthday;
 
     /**
-     * 性别(字典 1男 2女 3未知)
+     * 性别（M-男，F-女）
      */
     private String sex;
 
