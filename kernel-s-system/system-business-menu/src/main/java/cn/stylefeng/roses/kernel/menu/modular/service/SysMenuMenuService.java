4f13607934f857a245febf9bb8e6d0ad58ecbed6
q18idc <admin@q18idc.com>
2021-01-09 11:07:44 +0800

    【system】添加菜单按钮实体、service、mapper

---
+++ kernel-s-system/system-business-menu/src/main/java/cn/stylefeng/roses/kernel/menu/modular/service/SysMenuMenuService.java
@@ -0,0 +1,14 @@
+package cn.stylefeng.roses.kernel.menu.modular.service;
+
+import cn.stylefeng.roses.kernel.menu.modular.entity.SysMenuButton;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * 系统菜单按钮service接口
+ *
+ * @author q18idc.com QQ993143799
+ * @date 2021/1/9 11:04
+ */
+public interface SysMenuMenuService extends IService<SysMenuButton> {
+
+}
---
+++ kernel-s-system/system-business-menu/src/main/java/cn/stylefeng/roses/kernel/menu/modular/service/impl/SysMenuMenuServiceImpl.java
@@ -0,0 +1,16 @@
+package cn.stylefeng.roses.kernel.menu.modular.service.impl;
+
+import cn.stylefeng.roses.kernel.menu.modular.entity.SysMenuButton;
+import cn.stylefeng.roses.kernel.menu.modular.mapper.SysMenuButtonMapper;
+import cn.stylefeng.roses.kernel.menu.modular.service.SysMenuMenuService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+/**
+ * 系统菜单按钮service接口实现类
+ *
+ * @author q18idc.com QQ993143799
+ * @date 2021/1/9 11:05
+ */
+public class SysMenuMenuServiceImpl extends ServiceImpl<SysMenuButtonMapper, SysMenuButton> implements SysMenuMenuService {
+
+}
