a40843fb8b36772c7dc26f89fa7ab22fc1d7fac6
chenjinlong <22208488@qq.com>
2021-01-26 17:01:33 +0800

    【DictController】增加获取多语言字典列表

--- kernel-s-dict/dict-business/src/main/java/cn/stylefeng/roses/kernel/dict/modular/controller/DictController.java
+++ kernel-s-dict/dict-business/src/main/java/cn/stylefeng/roses/kernel/dict/modular/controller/DictController.java
@@ -21,6 +21,7 @@ import javax.annotation.Resource;
 import java.util.List;
 
 import static cn.stylefeng.roses.kernel.dict.api.constants.DictConstants.CONFIG_GROUP_DICT_TYPE_CODE;
+import static cn.stylefeng.roses.kernel.dict.api.constants.DictConstants.LANGUAGES_DICT_TYPE_CODE;
 
 /**
  * 字典详情管理，具体管理某个字典类型下的条目
@@ -142,6 +143,19 @@ public class DictController {
     @GetResource(name = "获取系统配置分组字典列表", path = "/dict/getConfigGroupPage", requiredPermission = false)
     public ResponseData getConfigGroupPage(DictRequest dictRequest) {
         dictRequest.setDictTypeCode(CONFIG_GROUP_DICT_TYPE_CODE);
+        PageResult<SysDict> page = this.dictService.findPageList(dictRequest);
+        return new SuccessResponseData(page);
+    }
+
+    /**
+     * 获取多语言字典列表(分页)
+     *
+     * @author chenjinlong
+     * @date 2021/1/25 11:47
+     */
+    @GetResource(name = "获取多语言字典列表", path = "/dict/getLanguagesPage", requiredPermission = false)
+    public ResponseData getLanguagesPage(DictRequest dictRequest) {
+        dictRequest.setDictTypeCode(LANGUAGES_DICT_TYPE_CODE);
         PageResult<SysDict> page = this.dictService.findPageList(dictRequest);
         return new SuccessResponseData(page);
     }
