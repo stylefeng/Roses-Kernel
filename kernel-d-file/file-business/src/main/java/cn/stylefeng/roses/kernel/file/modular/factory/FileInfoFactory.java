80389219002f545f4989a48685cf42b7d3251e62
fengshuonan <sn93@qq.com>
2021-04-13 18:52:41 +0800

    【7.0.3】更新文件上传

--- kernel-d-file/file-business/src/main/java/cn/stylefeng/roses/kernel/file/modular/factory/FileInfoFactory.java
+++ kernel-d-file/file-business/src/main/java/cn/stylefeng/roses/kernel/file/modular/factory/FileInfoFactory.java
@@ -96,6 +96,7 @@ public class FileInfoFactory {
 
         // 封装存储文件信息（上传替换公共信息）
         SysFileInfo sysFileInfo = new SysFileInfo();
+        sysFileInfo.setFileId(fileId);
         sysFileInfo.setFileLocation(FileLocationEnum.LOCAL.getCode());
         sysFileInfo.setFileBucket(DEFAULT_BUCKET_NAME);
         sysFileInfo.setFileObjectName(finalFileName);
