f8107854ba57cc061bbdd18144d4f47a2e6a90b9
fengshuonan <sn93@qq.com>
2021-04-20 20:12:51 +0800

    【7.0.3】整理版本号

--- kernel-d-seata/seata-demo-order/src/main/java/cn/stylefeng/roses/kernel/seata/order/controller/OrderController.java
+++ kernel-d-seata/seata-demo-order/src/main/java/cn/stylefeng/roses/kernel/seata/order/controller/OrderController.java
@@ -2,8 +2,9 @@ package cn.stylefeng.roses.kernel.seata.order.controller;
 
 import cn.stylefeng.roses.kernel.seata.order.entity.Order;
 import cn.stylefeng.roses.kernel.seata.order.service.OrderService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.annotation.Resource;
 
 /**
  * 订单接口
@@ -13,15 +14,18 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 public class OrderController {
 
-    @Autowired
-    OrderService orderService;
+    @Resource
+    private OrderService orderService;
 
     /**
      * 创建订单
+     *
+     * @author wangyl
+     * @date 2021/4/20 20:11
      */
     @GetMapping("/create")
-    Order create(String userId, String commodityCode, int orderCount){
-        return orderService.create(userId,commodityCode,orderCount);
+    public Order create(String userId, String commodityCode, int orderCount) {
+        return orderService.create(userId, commodityCode, orderCount);
     }
 
 }
