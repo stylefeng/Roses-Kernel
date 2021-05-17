ac9a0dbf402705dbe20dd8c2b6b9655f194b5040
fengshuonan <sn93@qq.com>
2021-05-17 12:05:47 +0800

    【7.0.4】更新自动配置顺序

--- kernel-s-system/system-integration-beetl/src/main/java/cn/stylefeng/roses/kernel/system/integration/config/BeetlErrorViewAutoConfiguration.java
+++ kernel-s-system/system-integration-beetl/src/main/java/cn/stylefeng/roses/kernel/system/integration/config/BeetlErrorViewAutoConfiguration.java
@@ -2,6 +2,8 @@ package cn.stylefeng.roses.kernel.system.integration.config;
 
 import cn.stylefeng.roses.kernel.system.integration.CustomErrorView;
 import cn.stylefeng.roses.kernel.system.integration.core.CustomBeetlGroupUtilConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
  * @date 2021/5/17 11:16
  */
 @Configuration
+@AutoConfigureBefore(ErrorMvcAutoConfiguration.class)
 public class BeetlErrorViewAutoConfiguration {
 
     /**
--- kernel-s-system/system-integration-rest/src/main/java/cn/stylefeng/roses/kernel/system/integration/config/RestErrorViewAutoConfiguration.java
+++ kernel-s-system/system-integration-rest/src/main/java/cn/stylefeng/roses/kernel/system/integration/config/RestErrorViewAutoConfiguration.java
@@ -1,6 +1,8 @@
 package cn.stylefeng.roses.kernel.system.integration.config;
 
 import cn.stylefeng.roses.kernel.system.integration.ErrorStaticJsonView;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
  * @date 2021/5/17 11:16
  */
 @Configuration
+@AutoConfigureBefore(ErrorMvcAutoConfiguration.class)
 public class RestErrorViewAutoConfiguration {
 
     /**
