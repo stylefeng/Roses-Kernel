a1ed6c373b6070a59215ed6a656d987031a7a3c9
fengshuonan <sn93@qq.com>
2021-07-10 22:21:45 +0800

    【7.0.4】【bugfix】修复api注解的默认请求方法

--- kernel-d-scanner/scanner-api/src/main/java/cn/stylefeng/roses/kernel/scanner/api/annotation/ApiResource.java
+++ kernel-d-scanner/scanner-api/src/main/java/cn/stylefeng/roses/kernel/scanner/api/annotation/ApiResource.java
@@ -41,7 +41,7 @@ import java.lang.annotation.*;
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@RequestMapping(method = RequestMethod.POST)
+@RequestMapping
 public @interface ApiResource {
 
     /**
