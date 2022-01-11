2226ace7769067cab2c789ce491f217e44d01160
fengshuonan <sn93@qq.com>
2022-01-11 14:13:32 +0800

    【7.1.6】【scanner】优化资源汇报pojo

---
+++ kernel-d-scanner/scanner-api/src/main/java/cn/stylefeng/roses/kernel/scanner/api/pojo/resource/DevOpsReportResourceParam.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright [2020-2030] [https://www.stylefeng.cn]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Guns采用APACHE LICENSE 2.0开源协议，您在使用过程中，需要注意以下几点：
+ *
+ * 1.请不要删除和修改根目录下的LICENSE文件。
+ * 2.请不要删除和修改Guns源码头部的版权声明。
+ * 3.请保留源码和相关描述文件的项目出处，作者声明等。
+ * 4.分发源码时候，请注明软件出处 https://gitee.com/stylefeng/guns
+ * 5.在修改包名，模块名称，项目代码等时，请注明软件出处 https://gitee.com/stylefeng/guns
+ * 6.若您的项目无法满足以上几点，可申请商业授权
+ */
+package cn.stylefeng.roses.kernel.scanner.api.pojo.resource;
+
+import cn.stylefeng.roses.kernel.rule.pojo.request.BaseRequest;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.Map;
+
+/**
+ * 资源发送到DevOps一体化平台的参数
+ *
+ * @author fengshuonan
+ * @date 2022/1/11 14:05
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class DevOpsReportResourceParam extends BaseRequest {
+
+    /**
+     * 项目唯一编码，在DevOps平台创建项目后会颁发
+     */
+    private String projectUniqueCode;
+
+    /**
+     * 向DevOps平台发送资源时候的令牌
+     */
+    private String interactionToken;
+
+    /**
+     * 第一个key是模块名称，是下划线分割的控制器名称，不带Controller结尾
+     * <p>
+     * 第二个key是资源的编码
+     */
+    private Map<String, Map<String, ResourceDefinition>> resourceDefinitions;
+
+    public DevOpsReportResourceParam(String projectUniqueCode, String interactionToken, Map<String, Map<String, ResourceDefinition>> resourceDefinitions) {
+        this.projectUniqueCode = projectUniqueCode;
+        this.interactionToken = interactionToken;
+        this.resourceDefinitions = resourceDefinitions;
+    }
+
+}
