d352f325bf724a4c4f85fa8a9ce3f8857565ec4d
fengshuonan <sn93@qq.com>
2022-01-16 11:36:30 +0800

    【7.1.6】【scanner】更新app控制器接口扫描

--- kernel-s-system/system-business-app/src/main/java/cn/stylefeng/roses/kernel/system/modular/app/controller/SysAppController.java
+++ kernel-s-system/system-business-app/src/main/java/cn/stylefeng/roses/kernel/system/modular/app/controller/SysAppController.java
@@ -24,6 +24,7 @@
  */
 package cn.stylefeng.roses.kernel.system.modular.app.controller;
 
+import cn.stylefeng.roses.kernel.db.api.pojo.page.PageResult;
 import cn.stylefeng.roses.kernel.rule.pojo.response.ResponseData;
 import cn.stylefeng.roses.kernel.rule.pojo.response.SuccessResponseData;
 import cn.stylefeng.roses.kernel.scanner.api.annotation.ApiResource;
@@ -59,9 +60,9 @@ public class SysAppController {
      * @date 2020/3/25 14:44
      */
     @PostResource(name = "添加系统应用", path = "/sysApp/add")
-    public ResponseData add(@RequestBody @Validated(SysAppRequest.add.class) SysAppRequest sysAppParam) {
+    public ResponseData<?> add(@RequestBody @Validated(SysAppRequest.add.class) SysAppRequest sysAppParam) {
         sysAppService.add(sysAppParam);
-        return new SuccessResponseData();
+        return new SuccessResponseData<>();
     }
 
     /**
@@ -71,9 +72,9 @@ public class SysAppController {
      * @date 2020/3/25 14:54
      */
     @PostResource(name = "删除系统应用", path = "/sysApp/delete")
-    public ResponseData delete(@RequestBody @Validated(SysAppRequest.delete.class) SysAppRequest sysAppParam) {
+    public ResponseData<?> delete(@RequestBody @Validated(SysAppRequest.delete.class) SysAppRequest sysAppParam) {
         sysAppService.del(sysAppParam);
-        return new SuccessResponseData();
+        return new SuccessResponseData<>();
     }
 
     /**
@@ -83,9 +84,9 @@ public class SysAppController {
      * @date 2020/3/25 14:54
      */
     @PostResource(name = "编辑系统应用", path = "/sysApp/edit")
-    public ResponseData edit(@RequestBody @Validated(SysAppRequest.edit.class) SysAppRequest sysAppParam) {
+    public ResponseData<?> edit(@RequestBody @Validated(SysAppRequest.edit.class) SysAppRequest sysAppParam) {
         sysAppService.edit(sysAppParam);
-        return new SuccessResponseData();
+        return new SuccessResponseData<>();
     }
 
     /**
@@ -95,9 +96,9 @@ public class SysAppController {
      * @date 2020/6/29 16:49
      */
     @PostResource(name = "修改应用状态", path = "/sysApp/updateStatus")
-    public ResponseData updateStatus(@RequestBody @Validated(SysAppRequest.updateStatus.class) SysAppRequest sysAppParam) {
+    public ResponseData<?> updateStatus(@RequestBody @Validated(SysAppRequest.updateStatus.class) SysAppRequest sysAppParam) {
         sysAppService.editStatus(sysAppParam);
-        return new SuccessResponseData();
+        return new SuccessResponseData<>();
     }
 
     /**
@@ -107,8 +108,8 @@ public class SysAppController {
      * @date 2020/3/26 9:49
      */
     @GetResource(name = "查看系统应用", path = "/sysApp/detail")
-    public ResponseData detail(@Validated(SysAppRequest.detail.class) SysAppRequest sysAppParam) {
-        return new SuccessResponseData(sysAppService.detail(sysAppParam));
+    public ResponseData<SysApp> detail(@Validated(SysAppRequest.detail.class) SysAppRequest sysAppParam) {
+        return new SuccessResponseData<>(sysAppService.detail(sysAppParam));
     }
 
     /**
@@ -118,8 +119,8 @@ public class SysAppController {
      * @date 2020/4/19 14:55
      */
     @GetResource(name = "系统应用列表", path = "/sysApp/list")
-    public ResponseData list(SysAppRequest sysAppParam) {
-        return new SuccessResponseData(sysAppService.findList(sysAppParam));
+    public ResponseData<List<SysApp>> list(SysAppRequest sysAppParam) {
+        return new SuccessResponseData<>(sysAppService.findList(sysAppParam));
     }
 
     /**
@@ -129,8 +130,8 @@ public class SysAppController {
      * @date 2020/3/20 21:23
      */
     @GetResource(name = "查询系统应用", path = "/sysApp/page")
-    public ResponseData page(SysAppRequest sysAppParam) {
-        return new SuccessResponseData(sysAppService.findPage(sysAppParam));
+    public ResponseData<PageResult<SysApp>> page(SysAppRequest sysAppParam) {
+        return new SuccessResponseData<>(sysAppService.findPage(sysAppParam));
     }
 
     /**
@@ -140,9 +141,9 @@ public class SysAppController {
      * @date 2020/6/29 16:49
      */
     @PostResource(name = "设为默认应用", path = "/sysApp/updateActiveFlag")
-    public ResponseData setAsDefault(@RequestBody @Validated(SysAppRequest.updateActiveFlag.class) SysAppRequest sysAppParam) {
+    public ResponseData<?> setAsDefault(@RequestBody @Validated(SysAppRequest.updateActiveFlag.class) SysAppRequest sysAppParam) {
         sysAppService.updateActiveFlag(sysAppParam);
-        return new SuccessResponseData();
+        return new SuccessResponseData<>();
     }
 
     /**
@@ -152,9 +153,9 @@ public class SysAppController {
      * @date 2021/4/21 15:31
      */
     @GetResource(name = "获取应用列表，用于顶部应用列表", path = "/sysMenu/getTopAppList", requiredPermission = false)
-    public ResponseData getTopAppList() {
+    public ResponseData<List<SysApp>> getTopAppList() {
         List<SysApp> userTopAppList = sysAppService.getUserTopAppList();
-        return new SuccessResponseData(userTopAppList);
+        return new SuccessResponseData<>(userTopAppList);
     }
 
 }
