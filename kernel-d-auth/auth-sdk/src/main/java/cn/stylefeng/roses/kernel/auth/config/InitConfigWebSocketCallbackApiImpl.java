fc783bbdf6beb7362f17218a796b033381373c2e
fengshuonan <sn93@qq.com>
2022-03-15 16:36:35 +0800

    【7.2.2】更新注解

--- kernel-d-auth/auth-sdk/src/main/java/cn/stylefeng/roses/kernel/auth/config/InitConfigWebSocketCallbackApiImpl.java
+++ kernel-d-auth/auth-sdk/src/main/java/cn/stylefeng/roses/kernel/auth/config/InitConfigWebSocketCallbackApiImpl.java
@@ -5,8 +5,9 @@ import cn.stylefeng.roses.kernel.auth.api.context.LoginContext;
 import cn.stylefeng.roses.kernel.auth.api.pojo.login.LoginUser;
 import cn.stylefeng.roses.kernel.config.api.ConfigInitCallbackApi;
 import cn.stylefeng.roses.kernel.message.api.expander.WebSocketConfigExpander;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 
 /**
  * 项目初始化完成以后，修改用户websocket地址的配置
@@ -17,7 +18,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class InitConfigWebSocketCallbackApiImpl implements ConfigInitCallbackApi {
 
-    @Autowired
+    @Resource
     private SessionManagerApi sessionManagerApi;
 
     @Override
